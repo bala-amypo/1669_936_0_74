49769dd36efdc7e989f0b40133200eebc7d8a952
bala-amypo <bala@amypo.in>
2025-12-17 08:27:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import java.util.*;
-
-
-public   StudentRepo{
-
-} 
---
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import java.util.*;
+
+@Repository
+public Iterface StudentRepository extends JpaRepository{
+    
+} 
