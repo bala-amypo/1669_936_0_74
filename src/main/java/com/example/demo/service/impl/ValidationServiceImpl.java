e22c25198b0d284095fb83a936bca1fe1aee4994
bala-amypo <bala@amypo.in>
2025-12-18 08:54:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ValidationServiceImpl.java
@@ -1,1 +1,20 @@
-package com.example.demo.service.impl
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.service.import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.service.StudentService;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepository;Service;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepository;
+public class ValidationServiceImpl implements ValidationService{
+    @Autowired ValidationRepo repo;
+    @Override
+    public ValidationEntity postValid(ValidationEntity val){
+        return repo.save(val);
+    }
+}
