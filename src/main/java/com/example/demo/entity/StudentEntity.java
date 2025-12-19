6b38f3af1846fa74665e79a59f85f9933619ca9d
bala-amypo <bala@amypo.in>
2025-12-19 08:37:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,12 +1,15 @@
 package com.example.demo.entity;
 
-import java.util.*;
+import java.util.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
-
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+import lombok.Getter;
 @Entity
 @Data
 @Getter
--- src/main/java/com/example/demo/service/TimeStampService.java
+++ src/main/java/com/example/demo/service/TimeStampService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.TimeStampEntity;
-import java.util.List;
 public interface TimeStampService{
     TimeStampEntity postStamp(TimeStampEntity time);
    
