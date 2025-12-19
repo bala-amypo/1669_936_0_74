b0820e23f539062b079c27f1784dac57868436c2
bala-amypo <bala@amypo.in>
2025-12-19 08:43:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,6 +1,6 @@
 package com.example.demo.entity;
 
-import java.util.LocalDateTime;
+import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/example/demo/service/impl/TimeStampServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TimeStampServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.TimeStampEntity;
+import com.example.demo.service.TimeStampService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.repository.TimeStampRepository;
