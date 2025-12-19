25aa1ed9e429b06300b57d55dc3cbde3407080dd
bala-amypo <bala@amypo.in>
2025-12-19 08:46:05 +0000

    User commit

--- src/main/java/com/example/demo/controller/TimeStampController.java
+++ src/main/java/com/example/demo/controller/TimeStampController.java
@@ -1,15 +1,12 @@
 package com.example.demo.controller;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.service.TimeStampService;
 import com.example.demo.entity.TimeStampEntity;
-import jakarta.validation.Valid;
 @RestController
 public class TimeStampController{
     @Autowired TimeStampService serve;
--- src/main/java/com/example/demo/repository/TimeStampRepository.java
+++ src/main/java/com/example/demo/repository/TimeStampRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ValidationEntity;
+import com.example.demo.entity.TimeStampEntity;
 
 @Repository
 public interface TimeStampRepository extends JpaRepostiory<TimeStampEntity,Long>{
