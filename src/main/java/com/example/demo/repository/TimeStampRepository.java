9cdf80cc500250cdfd3b714e06a03831e7dcf217
bala-amypo <bala@amypo.in>
2025-12-19 08:04:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/TimeStampRepository.java
+++ src/main/java/com/example/demo/repository/TimeStampRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
-import org.springframework.web.bind.annotation.Repository;
-import org.springframework.web.bind.annotation.Repository;
-import org.springframework.web.bind.annotation.Repository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.ValidationEntity;
 
 @Repository
-public TimeS
+public interface TimeStampRepository extends JpaRepostiory<TimeStampEntity,Long>{
+
+}
---
+++ src/main/java/com/example/demo/service/ValidationService copy.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.ValidationEntity;
+import java.util.List;
+public interface ValidationService{
+    ValidationEntity postValid(ValidationEntity val);
+    ValidationEntity findData(Long id);
+}
