3124397323176af94de048fdd1ca1821eae04e53
bala-amypo <bala@amypo.in>
2025-12-19 08:01:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/TimeStampRepository.java
+++ src/main/java/com/example/demo/repository/TimeStampRepository.java
@@ -1,1 +1,8 @@
-package com.example.demo.
+package com.example.demo.repository;
+
+import org.springframework.web.bind.annotation.Repository;
+import org.springframework.web.bind.annotation.Repository;
+import org.springframework.web.bind.annotation.Repository;
+
+@Repository
+public TimeS
