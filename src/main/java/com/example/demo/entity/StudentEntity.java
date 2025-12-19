37663be5a076dd49115ea3148ffcf900bb764ab1
bala-amypo <bala@amypo.in>
2025-12-19 06:26:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -16,44 +16,5 @@ public class StudentEntity{
     private String email; 
     private String password;
     private Date created;
-    public Integer getId(){
-        return id;
-    }
-    public void setId(Integer id){
-        this.id=id;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(){
-        this.password=password;
-    }
-    public Date getCreated(){
-        return created;
-    }
-    public void setCreated(Date created){
-        this.created=created;
-    }
-    public StudentEntity(){
-
-    }
-    public StudentEntity(Integer id,String name,String email,String password,Date created){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        this.created=created; 
-    }
+    
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -27,47 +27,47 @@ public class ValidationEntity{
     @Max(30) @Positive(message="Age must be a Positive number")
     private int age;
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public void setId(String username){
-        this.username=username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public int getAge(){
-        return age;
-    }
-    public void setId(int age){
-        this.age=age;
-    }
-    public ValidationEntity(Long id,@NotNull @Size(min=2,max=10,message="must be 2 to 10 characters") String username,
-    @Email(message="Email is valid") String email,
-    @Size(min=2,max=10,message="must be 2 to 10 characters") @NotNull(message="Password is mandatory") String password,
-    @Max(30) @Positive(message="Age must be a Positive number") int age){
-        this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-    }
-    public ValidationEntity(){
+    // public Long getId(){
+    //     return id;
+    // }
+    // public void setId(Long id){
+    //     this.id=id;
+    // }
+    // public String getUsername(){
+    //     return username;
+    // }
+    // public void setId(String username){
+    //     this.username=username;
+    // }
+    // public String getEmail(){
+    //     return email;
+    // }
+    // public void setEmail(String email){
+    //     this.email=email;
+    // }
+    // public String getPassword(){
+    //     return password;
+    // }
+    // public void setPassword(String password){
+    //     this.password=password;
+    // }
+    // public int getAge(){
+    //     return age;
+    // }
+    // public void setId(int age){
+    //     this.age=age;
+    // }
+    // public ValidationEntity(Long id,@NotNull @Size(min=2,max=10,message="must be 2 to 10 characters") String username,
+    // @Email(message="Email is valid") String email,
+    // @Size(min=2,max=10,message="must be 2 to 10 characters") @NotNull(message="Password is mandatory") String password,
+    // @Max(30) @Positive(message="Age must be a Positive number") int age){
+    //     this.id=id;
+    //     this.username=username;
+    //     this.email=email;
+    //     this.password=password;
+    //     this.age=age;
+    // }
+    // public ValidationEntity(){
         
-    }
+    // }
 }
