c4c2036c533dcf92692e318fcd9072e94282541d
bala-amypo <bala@amypo.in>
2025-12-23 16:25:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PersonServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PersonServiceImpl.java
@@ -2,13 +2,15 @@ package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 import com.example.demo.entity.Person;
+import com.example.demo.entity.Course;
 import com.example.demo.repository.PersonRepository;
+import com.example.demo.repository.CourseRepository;
 import com.example.demo.service.PersonService;
 
 @Service
-public class PersonService {
+public class PersonServiceImpl implements PersonService {
 
     @Autowired
     private PersonRepository personRepo;
