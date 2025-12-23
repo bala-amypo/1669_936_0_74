a2b57f23fac7d13816e443fc600058070ce00bf6
bala-amypo <bala@amypo.in>
2025-12-23 15:37:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -2,29 +2,44 @@ package com.example.demo.entity;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Department {
-
      @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long person_id;
 
-      private Long dep_id;
-    private String dep_name;
-    private String dep_head;
+     @NotNull
+     @Email
+     @Column(unique=true)
+    private String email;
+        @NotNull
+    private String person_name;
 
-    @OneToMany(mappedBy = "")
- private List<Student> stu; 
+    @ManyToMany
+    @JoinTable(
+        name="Common_Table",
+        joinColumns=@JoinColumn(name="person_id"), 
+        inverseJoinColumns = @JoinColumn(name="course_id") 
+        
+    )             
+    private List<Course> course;
+    
 
 }
